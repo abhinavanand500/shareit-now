47b5b2744872cae9c088e1c41c192c5b525e4c5b
Abhinav Anand <abhinav.anand500@gmail.com>
2021-06-12 18:23:38 +0530

    Done with mail problem

--- services/emailServices.js
+++ services/emailServices.js
@@ -1,22 +1,22 @@
 const nodemailer = require("nodemailer");
-
 module.exports = async ({ from, to, subject, text, html }) => {
+    console.log("Hiiiiii");
     let transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
         port: process.env.SMTP_PORT,
-        secure: false,
+        secure: false, // true for 465, false for other ports
         auth: {
-            user: process.env.MAIL_USER,
-            pass: process.env.MAIL_PASS,
+            user: process.env.MAIL_USER, // generated ethereal user
+            pass: process.env.MAIL_PASS, // generated ethereal password
         },
     });
-
+    console.log("Byeeee");
+    // send mail with defined transport object
     let info = await transporter.sendMail({
-        from: `ShareKaro <${from}>`,
-        to: to,
-        subject: subject,
-        text: text,
-        html: html,
+        from: `inShare <${from}>`, // sender address
+        to: to, // list of receivers
+        subject: subject, // Subject line
+        text: text, // plain text body
+        html: html, // html body
     });
-    console.log(info);
 };
