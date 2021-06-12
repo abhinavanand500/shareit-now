82b3ff482b0d753d7e9846a3418d39f3d013ea3e
Abhinav Anand <abhinav.anand500@gmail.com>
2021-06-12 21:01:13 +0530

    Done

--- routes/files.js
+++ routes/files.js
@@ -20,6 +20,7 @@ let upload = multer({ storage, limits: { fileSize: 1000000 * 100 } }).single(
 
 router.post("/", (req, res) => {
     // Store Files
+    console.log("Hello");
     upload(req, res, async (err) => {
         // Validate Request
         if (!req.file) {
